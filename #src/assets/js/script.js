bf1e25c4e2d7d3e13b13bcbffb407e9cbf9bef26
GrafGer <mr.gerantev@mail.ru>
2020-11-07 20:23:32 +0300

    Закончил Section2

--- #src/assets/js/script.js
+++ #src/assets/js/script.js
@@ -2,6 +2,23 @@ $(function () {
 	$('.menu__btn').on('click', function() {
 		$(this).toggleClass('menu__btn--active');	
 		$('.header-menu__inner').toggleClass('header-menu__inner--active');
+		if($(this).hasClass('menu__btn--active')) {
+				$("html,body").css("overflow","hidden");
+		} else {
+		$("html,body").css("overflow","visible");
+		}
 
 	});
+
+/* 	$(window).on('scroll',function () {
+
+
+			if ($(this).scrollTop() > 500) {
+					$('.header').addClass('header--fixed');
+			}
+			else {
+					$('.header').removeClass('header--fixed');
+			}
+	}); */
+
 });
